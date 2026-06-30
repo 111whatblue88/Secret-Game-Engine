b1dd357cecddec3537e6fe8bb380125a67ea9dab
elo <EloOfTheWired@proton.me>
2026-06-29 18:52:41 -0700

    edited base template

--- engine/template/src/main.cpp
+++ engine/template/src/main.cpp
@@ -1,20 +1,13 @@
-#include <string>
-
 #include "../../../engine/src/secret.hpp"
 
 int main() {
 
   using namespace secret;
-  using namespace input;
-  using namespace ecs;
-  using namespace rend;
-  using namespace console;
-  using ID = uint32_t;
-
   const std::string GAMENAME = "{{NAME}}";
 
   core::Engine::init(500, 500, GAMENAME);
   core::Engine::options.fpsCap = 60;
+  core::Engine::options.renderingAPI = core::RenderingAPIs::SDL;
 
   // Update - Called every frame
   core::Engine::update = [&]() {
