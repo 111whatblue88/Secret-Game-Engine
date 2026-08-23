eb0b8f4212d86ea81173e427a3cf39fde82fc5c5
elo <EloOfTheWired@proton.me>
2026-08-23 01:45:02 -0700

    fixed glew path

--- engine/template/premake5.lua
+++ engine/template/premake5.lua
@@ -27,7 +27,7 @@ project "{{NAME}}"
       "../../engine/vendored/SDL/src_mixer/build/Debug",
       "../../engine/vendored/SDL/src_ttf/build/Debug",
       "../../engine/vendored/SDL/src_image/build/Debug",
-			"../../engine/vendored/glew/build/cmake/build/lib/Debug",
+			"../../engine/vendored/non-git/glew/build/cmake/build/lib/Debug",
       "../../build/secret/lib", 
 			}
 		links { "SDL3", "SDL3_ttf", "SDL3_image", "SDL3_mixer", "user32", "glew32d", "opengl32", "secret" }
